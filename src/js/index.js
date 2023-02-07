e9e54025ff8329c29d072fd343080e405ebe8b82
moncherry369 <ssgarrido1994@gmail.com>
2023-02-07 23:07:12 +0000

    Final completion of traffic light glow->selected

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,50 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-import "../styles/index.scss"
-
-
-const TrafficLight= () => {1
-    const [state,setState] = useState(".red");
-    const [light,setLight] = useState(true);
-    
-    const toggleLight = () => {
-        //this isn't working need to make glow on select//
-        setLight((curr) => (curr == setState ? "": state));
-    }
-    return (
-        <div>
-            
-                <div className="top"></div>
-            <div className="container">
-                <div className="red light"></div>
-                <div className="yellow light"></div>
-                <div className="green light"></div>
-            </div>
-        </div>
-        
-        )
+//import react into the bundle
+import React, { useState, useEffect } from "react";
+import ReactDOM, { render } from "react-dom";
+// include your styles into the webpack bundle
+import "../styles/index.scss";
+//import your own components
+import Home from "./component/home.jsx";
+//render your react application
+ReactDOM.render(<Home />, document.querySelector("#app"));
+function TrafficLight() {
+  const [state, setState] = useState();
+  const [light, setLight] = useState(true);
+  const toggleLight = () => {
+    setLight((curr) => (curr == state ? "" : state));
+  };
+  let selectedColor = "selected";
+  let redColor = "";
+  let yellowColor = "";
+  let greenColor = "";
+  if (state == "red") {
+    redColor = selectedColor;
+  }
+  if (state == "yellow") {
+    yellowColor = selectedColor;
+  }
+  if (state == "green") {
+    greenColor = selectedColor;
+  }
+  console.log(state);
+  return (
+    <>
+      <div id="top"></div>
+      <div id="container">
+        <div
+          className={`red light ${redColor}`}
+          onClick={() => setState("red")}
+        ></div>
+        <div
+          className={`yellow light ${yellowColor}`}
+          onClick={() => setState("yellow")}
+        ></div>
+        <div
+          className={`green light ${greenColor}`}
+          onClick={() => setState("green")}
+        ></div>
+      </div>
+    </>
+  );
 }
-ReactDOM.render(<TrafficLight />, document.querySelector('#app'));
+ReactDOM.render(<TrafficLight />, document.querySelector("#app"));
